22a9569ef69aa73011b144e453720e7104286619
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2025-03-28 00:20:09 +0530

    Create catalog-service project

---
+++ catalog-service/src/test/java/com/satyamagrawal/bookstore/catalogservice/TestCatalogServiceApplication.java
@@ -0,0 +1,11 @@
+package com.satyamagrawal.bookstore.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
