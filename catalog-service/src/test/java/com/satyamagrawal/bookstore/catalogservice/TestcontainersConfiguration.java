23c6d9752d36c7163c35be1145d3d595a312e9f3
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2025-03-29 11:43:35 +0530

    Add spotless plugin to the root project

--- catalog-service/src/test/java/com/satyamagrawal/bookstore/catalogservice/TestcontainersConfiguration.java
+++ catalog-service/src/test/java/com/satyamagrawal/bookstore/catalogservice/TestcontainersConfiguration.java
@@ -1,17 +1,3 @@
 package com.satyamagrawal.bookstore.catalogservice;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
-    }
-}
+class TestcontainersConfiguration {}
