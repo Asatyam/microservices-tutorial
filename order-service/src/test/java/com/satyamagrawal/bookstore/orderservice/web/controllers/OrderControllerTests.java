ec82fd8b4ad20d4ea36f44854df2a0b87e0d8c23
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2025-03-31 00:12:18 +0530

    Add tests for order-service

--- order-service/src/test/java/com/satyamagrawal/bookstore/orderservice/web/controllers/OrderControllerTests.java
+++ order-service/src/test/java/com/satyamagrawal/bookstore/orderservice/web/controllers/OrderControllerTests.java
@@ -1,4 +1,68 @@
 package com.satyamagrawal.bookstore.orderservice.web.controllers;
 
-public class OrderControllerTests {
+import com.satyamagrawal.bookstore.orderservice.AbstractIT;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import com.satyamagrawal.bookstore.orderservice.testdata.TestDataFactory;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class OrderControllerTests extends AbstractIT {
+
+    @Nested
+    class CreateOrderTests  {
+        @Test
+        void shouldCreateOrderSuccessfully() {
+            var payload =
+                    """
+                            {
+                                "customer": {
+                                    "name":"Satyam",
+                                    "email":"abcd@gmail.com",
+                                    "phone": "123457890"
+                                },
+                                "deliveryAddress": {
+                                    "addressLine1": "adr1",
+                                    "addressLine2": "adr2",
+                                    "city": "c1",
+                                    "state":"s1",
+                                    "zipCode":"123456",
+                                    "country":"In"
+                                },
+                                "items": [
+                                    {
+                                        "code": "P100",
+                                        "name": "Product 1",
+                                        "price": 25.50,
+                                        "quantity": 1
+                            
+                                    }
+                                ]
+                            }
+                            
+                            """;
+
+            given().contentType(ContentType.JSON)
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.CREATED.value())
+                    .body("orderNumber", notNullValue());
+        }
+
+        @Test
+        void shouldReturnBadRequestWhenMandatoryDataIsMissing() {
+            var payload = TestDataFactory.createOrderRequestWithInvalidCustomer();
+            given().contentType(ContentType.JSON)
+                    .body(payload)
+                    .when()
+                    .post("/api/orders")
+                    .then()
+                    .statusCode(HttpStatus.BAD_REQUEST.value());
+
+        }
+    }
 }
--- order-service/src/test/java/com/satyamagrawal/bookstore/orderservice/web/controllers/OrderControllerUnitTests.java
+++ order-service/src/test/java/com/satyamagrawal/bookstore/orderservice/web/controllers/OrderControllerUnitTests.java
@@ -1,4 +1,70 @@
 package com.satyamagrawal.bookstore.orderservice.web.controllers;
 
+import com.satyamagrawal.bookstore.orderservice.domain.OrderService;
+import com.satyamagrawal.bookstore.orderservice.domain.SecurityService;
+import com.satyamagrawal.bookstore.orderservice.domain.models.CreateOrderRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.satyamagrawal.bookstore.orderservice.testdata.TestDataFactory.*;
+import static org.junit.jupiter.api.Named.named;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.stream.Stream;
+
+
+@WebMvcTest(OrderController.class)
 public class OrderControllerUnitTests {
+    @MockitoBean
+    private OrderService orderService;
+
+    @MockitoBean
+    private SecurityService securityService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        given(securityService.getLoginUserName()).willReturn("satyam");
+    }
+
+    @ParameterizedTest(name="[{index}] {0}")
+    @MethodSource("createOrderRequestProvider")
+    void shouldReturnBadRequestWhenOrderPayloadIsInvalid(CreateOrderRequest request) throws Exception {
+        given(orderService.createOrder(eq("satyam"), any(CreateOrderRequest.class)))
+                .willReturn(null);
+        mockMvc.perform(post("/api/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    static Stream<Arguments> createOrderRequestProvider() {
+        return Stream.of(
+                arguments(named("Order with Invalid customer", createOrderRequestWithInvalidCustomer())),
+                arguments(named("Order with Invalid Delivery Address", createOrderRequestWithInvalidDeliveryAddress())),
+                arguments(named("Order with No Items", createOrderRequestWithNoItems()))
+        );
+    }
+
+
 }
